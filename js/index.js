a274fa2d036e9ea26b51031de7b0080779dc6d7e
mornicamwende <mornicamwende@gmail.com>
2020-07-20 00:16:46 +0300

    add a paragraph a header and a table

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 function akaned(){
     event.preventDefault();
     var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-                    //  0         1           2           3           4           5           6
     var maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-                    //      0       1           2       3       4       5       6
     var femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-                    //       0         1       2       3       4       5       6
     var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
     var year = document.getElementById("year").value;
